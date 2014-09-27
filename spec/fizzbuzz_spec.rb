a2c8fadcecf9ece0b3b7dca74536acae32c72cb1
Hadi <hadichalabi@gmail.com>
2014-09-27 12:11:16 +0100

    Refactored Tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,27 +4,36 @@ describe 'Fizzbuzz' do
 
 	let(:fizzbuzz){Fizzbuzz.new}
 
-	it "is divisible by 3" do
-	expect(fizzbuzz.is_divisible_by_3?(3)).to eq(true)
-	end
+context "dividing numbers" do
+	
+		context "knows when a number is divisble by" do
 
-	it "it is divisible 5" do
-	expect(fizzbuzz.is_divisible_by_5?(5)).to eq(true)
-	end
+		it "3" do
+		expect(fizzbuzz.is_divisible_by_3?(3)).to eq(true)
+		end
 
-	it "is divisible by 15" do 
-	expect(fizzbuzz.is_divisible_by_15?(15)).to eq(true)
-	end
+		it "5" do
+		expect(fizzbuzz.is_divisible_by_5?(5)).to eq(true)
+		end
 
-	it "knows 3 is not divisble by 1 " do
-	expect(fizzbuzz.is_divisible_by_3?(1)).to eq(false)
+		it "15" do 
+		expect(fizzbuzz.is_divisible_by_15?(15)).to eq(true)
+		end
 	end
+end
 
-	it "knows 5 is not divisible by 1" do
-	expect(fizzbuzz.is_divisible_by_5?(1)).to eq(false)
-	end
+		context "knows when a number is not divisible by" do
 
-	it "knows 15 is not divisible by 1" do
-	expect(fizzbuzz.is_divisible_by_15?(1)).to eq(false)
+		it "3 " do
+		expect(fizzbuzz.is_divisible_by_3?(1)).to eq(false)
+		end
+
+		it "5" do
+		expect(fizzbuzz.is_divisible_by_5?(1)).to eq(false)
+		end
+
+		it "15" do
+		expect(fizzbuzz.is_divisible_by_15?(1)).to eq(false)
+		end
 	end
 end
